3487526af223e374a166c578e2b08a3b46926e8b
pablohr15 <pablitoherreruela15@gmail.com>
2023-05-29 00:51:08 +0200

    Se añade el uso de Calendar para seleccionar los días que se actualizará la BBDD

--- src/main/java/com/f1/DemoF1Application.java
+++ src/main/java/com/f1/DemoF1Application.java
@@ -3,6 +3,11 @@ package com.f1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static java.lang.Thread.sleep;
+
 @SpringBootApplication()
 public class DemoF1Application {
 
@@ -14,9 +19,21 @@ public class DemoF1Application {
 
 		SpringApplication.run(DemoF1Application.class, args);
 		System.out.println("Application starting now...");
+
 		WebScrapping ws = new WebScrapping();
-		System.out.println("Running web scraping");
-		ws.scraping();
+
+		Calendar c = new GregorianCalendar();
+		while (true){
+
+			if (c.get(Calendar.DAY_OF_WEEK)==1 && c.get(Calendar.HOUR_OF_DAY)>=22){
+				System.out.println("Running web scraping");
+				ws.scraping();
+			}
+		}
+
+
+
+
 	}
 
 }
